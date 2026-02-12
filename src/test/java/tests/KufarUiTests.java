597e3b8051b35c04775ae2ed75b14b8831e2420b
Roman Yarosh <yarosh0106@gmail.com>
2026-02-12 18:28:11 +0300

    deleted @Order

--- src/test/java/tests/KufarUiTests.java
+++ src/test/java/tests/KufarUiTests.java
@@ -12,7 +12,6 @@ import java.time.Duration;
 import static com.codeborne.selenide.Selenide.switchTo;
 import static com.codeborne.selenide.Selenide.webdriver;
 
-@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class KufarUiTests extends BaseTest {
 
     MainPage mainPage = new MainPage();
@@ -20,8 +19,7 @@ public class KufarUiTests extends BaseTest {
 
 
     @Test
-    @Order(1)
-    @DisplayName("1. Поиск товара и проверка текста")
+    @DisplayName("Поиск товара и проверка текста")
     void searchIphoneTest() {
         String query = utils.PropertyReader.getProperty("search.query");
         mainPage.search(query);
@@ -31,8 +29,7 @@ public class KufarUiTests extends BaseTest {
     }
 
     @Test
-    @Order(2)
-    @DisplayName("2. Переход по вкладке Авто и проверка URL")
+    @DisplayName("Переход по вкладке Авто и проверка URL")
     void tabNavigationTest() {
         mainPage.clickHeaderTab("Авто");
         switchTo().window(1);
@@ -40,8 +37,7 @@ public class KufarUiTests extends BaseTest {
     }
 
     @Test
-    @Order(3)
-    @DisplayName("3. Поиск некорректных данных")
+    @DisplayName("Поиск некорректных данных")
     void invalidSearchDataTest() {
         String invalidQuery = PropertyReader.getProperty("search.invalid");
 
@@ -50,16 +46,14 @@ public class KufarUiTests extends BaseTest {
     }
 
     @Test
-    @Order(4)
-    @DisplayName("4. Переключение языка на белорусский в футере")
+    @DisplayName("Переключение языка на белорусский в футере")
     void languageSwitchTest() {
         mainPage.switchLanguage("BY");
         Assertions.assertEquals("Дапамога", mainPage.getHelpButtonText());
     }
 
     @Test
-    @Order(5)
-    @DisplayName("5. Выбор региона")
+    @DisplayName("Выбор региона")
     void regionSelectionTest() {
         String region = PropertyReader.getProperty("region.name");
 
